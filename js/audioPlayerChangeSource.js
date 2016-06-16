d8f1ad50ab83d63eef442879c946c49327741a02
raviguru123 <raviguruiitr@gmail.com>
2016-06-16 11:37:15 +0530

    add seekbar in audio player

--- js/audioPlayerChangeSource.js
+++ js/audioPlayerChangeSource.js
@@ -8,22 +8,29 @@ app.controller('audioCtrl', ['$scope', function($scope){
 	"https://gopartiesnew.s3-ap-southeast-1.amazonaws.com/6a49b601-3b9e-44cc-ab5f-00363b709eb0.mp3",
 	"http://gopartiesstatic.s3.amazonaws.com/images/1465887591_Veer Vaar-(Mr-Jatt.com).mp3",
 	"http://gopartiesstatic.s3.amazonaws.com/images/1465754562_yaariyan-2014-love-me-thoda-aur[x-songs.pk].mp3"];
-	
 	$scope.init=function(){
-
 		$scope.audioSource=$scope.audios[0];
+		$scope.player.volume=1;
+		
+	}
+	$scope.player=document.getElementById('player');
+	$scope.volume=100;
+	//$scope.player=$scope.volume;
+	$scope.init();
+	$scope.changeVolume=function(vol){
+		debugger;
+		if(vol>1)
+			vol=1;
+		$scope.player.volume=vol;
 	}
 
-	$scope.player=document.getElementById('player');
-	$scope.init();
+
 	$scope.next=function(){
 		$scope.player.pause()
 		$scope.player.removeAttribute('src');
 		$scope.player.load();
 		if($scope.audios.length-1>$scope.audioIndex)
 			$scope.audioIndex+=1;
-
-		
 		else
 			$scope.audioIndex=0;
 
@@ -58,6 +65,7 @@ app.controller('audioCtrl', ['$scope', function($scope){
 	}
 	$scope.forwardClick=function(evt){
 		var x = evt.pageX - $("#playerTest").offset().left;
+		$scope.progress=x/2;
 		$scope.player.currentTime=(($scope.player.duration)*x)/200;
 	}
 	$scope.player.addEventListener('ended', function(){
